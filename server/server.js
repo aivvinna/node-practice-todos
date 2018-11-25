35a51fde4f035638c76d886cb917ebc45a40a0f0
Hiroki Furuichi <aivvinna@gmail.com>
2018-11-25 20:55:04 +0900

    got started with mongoose

---
+++ server/server.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+const Todo = mongoose.model('Todo', {
+  text: {
+    type: String
+  },
+  completed: {
+    type: Boolean
+  },
+  completedAt: {
+    type: Number
+  }
+});
+
+// const newTodo = new Todo({
+//   text: 'Cook dinner'
+// })
+
+const newTodo = new Todo({
+  text: 'Do challenge',
+  completed: true,
+  completedAt: 1
+});
+
+newTodo.save().then((doc) => {
+  console.log('Saved todo', doc);
+}, (e) => {
+  console.log('Unable to save todo')
+});
