de19865d8fdb05a9f85618198deef208fabbd3b6
Hiroki Furuichi <aivvinna@gmail.com>
2018-11-30 02:03:59 +0900

    added GET /users/me and auth middleware

--- server/models/user.js
+++ server/models/user.js
@@ -42,12 +42,29 @@ UserSchema.methods.toJSON = function() {
 UserSchema.methods.generateAuthToken = function() {
   const user = this;
   const access = 'auth';
-  const token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString;
+  const token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
 
   user.tokens = user.tokens.concat([{access, token}]);
 
   return user.save().then(() => {
     return token;
+  })
+}
+
+UserSchema.statics.findByToken = function(token) {
+  const User = this;
+  let decoded;
+
+  try {
+    decoded = jwt.verify(token, 'abc123');
+  } catch (e) {
+    return Promise.reject();
+  }
+
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
   })
 }
 
