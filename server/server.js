5ed9fccbb81403c6df0a668861266de0bc5175a2
Hiroki Furuichi <aivvinna@gmail.com>
2018-12-12 23:18:35 +0900

    all api crud actions updated to use async await

--- server/server.js
+++ server/server.js
@@ -15,48 +15,48 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json())
 
-app.post('/todos', authenticate, (req, res) => {
-  const todo = new Todo({
-    text: req.body.text,
-    _creator: req.user._id
-  });
-
-  todo.save().then((doc) => {
+app.post('/todos', authenticate, async (req, res) => {
+  try {
+    const todo = new Todo({
+      text: req.body.text,
+      _creator: req.user._id
+    });
+    const doc = await todo.save();
     res.send(doc);
-  }, (e) => {
+  } catch(e) {
     res.status(400).send(e);
-  });
+  }
 });
 
-app.get('/todos', authenticate, (req, res) => {
-  Todo.find({
-    _creator: req.user._id
-  }).then((todos) => {
+app.get('/todos', authenticate, async (req, res) => {
+  try {
+    const todos = await Todo.find({_creator: req.user._id});
     res.send({todos});
-  }, () => {
+  } catch(e) {
     res.status(400).send(e);
-  })
+  }
 })
 
-app.get('/todos/:id', authenticate, (req, res) => {
+app.get('/todos/:id', authenticate, async (req, res) => {
   const id = req.params.id;
 
   if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
 
-  Todo.findOne({
-    _id: id,
+  const todo = await Todo.findOne({
+    _id: id, 
     _creator: req.user._id
-  }).then((todo) => {
-    if(!todo) {
+  });
+  try {
+  
+    if (!todo) {
       return res.status(404).send();
     }
-
     res.send({todo});
-  }).catch((e) => {
+  } catch(e) {
     res.status(400).send();
-  });
+  }
 });
 
 app.delete('/todos/:id', authenticate, async (req, res) => {
@@ -81,7 +81,7 @@ app.delete('/todos/:id', authenticate, async (req, res) => {
   }
 });
 
-app.patch('/todos/:id', authenticate, (req, res) => {
+app.patch('/todos/:id', authenticate, async (req, res) => {
   const id = req.params.id;
   const body = _.pick(req.body, ['text', 'completed']);
 
@@ -96,16 +96,25 @@ app.patch('/todos/:id', authenticate, (req, res) => {
     body.completedAt = null;
   }
 
-  Todo.findOneAndUpdate({_id: id, _creator: req.user._id}, {$set: body}, {new: true}).then((todo) => {
+  try {
+    const todo = await Todo.findOneAndUpdate({
+      _id: id,
+      _creator: req.user._id
+    }, {
+      $set: body
+    }, {
+      new: true
+    });
+
     if (!todo) {
       return res.status(404).send();
     }
 
     res.send({todo});
-  }).catch((e) => {
+  } catch(e) {
     res.status(400).send();
-  })
-})
+  }
+});
 
 app.post('/users', async (req, res) => {
   try {
